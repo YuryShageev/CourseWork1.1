8fa82487d8e21267212ca7599be6b9719575e730
YuryShageev <schageev.yur@yandex.ru>
2022-11-03 22:44:30 +0300

    Начинаю курсовую

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,19 @@
+import java.util.Objects;
+
 public class Employee {
-    private String fullName;
+    private final String fullName;
     private int salary;
     private int departments;
     private final int id;
-    private int counter;
+    private static int counter;
 
     public Employee(String fullName, int departments, int salary) {
-        this.fullName = fullName;
-        this.departments = departments;
-        this.salary = salary;
-        id = counter++;
+            this.fullName = fullName;
+            this.departments = departments;
+            this.salary = salary;
+            id = counter++;
     }
+
 
     public int getId() {
         return id;
@@ -39,5 +42,13 @@ public class Employee {
     @Override
     public String toString() {
         return "Сотрудник под номером: " + id + " По имени: " + fullName + " Отдела: " + departments + " Получает " + salary + " рублей";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return Objects.equals(fullName, employee.fullName) && Objects.equals(departments, employee.departments) && Objects.equals(salary, employee.salary);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10];
+
+        employees[0] = new Employee("Васечкин Пётр Сергеевич", 2, 89473);
+        employees[1] = new Employee("Клод Жан Вандамович", 1, 111384);
+        employees[2] = new Employee("Шварцнегер Арнольд Терминаторович", 4, 38912);
+
+        if (employees != null) {
+            for (int i = 0; i < employees.length; i++) {
+                System.out.println(employees[i].toString());
+            }
+        }
+//        for (int i = 0; i < employees.length; i++) {
+//            if (employees.length != 0) {
+//                System.out.println(employees[i].toString());
+//            }
+//        }
     }
 }
