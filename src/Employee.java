3d788dc92690ffc62fb27d0a2a6b932434657859
YuryShageev <schageev.yur@yandex.ru>
2022-11-03 17:49:55 +0300

    Начинаю курсовую

--- src/Employee.java
+++ src/Employee.java
@@ -11,4 +11,33 @@ public class Employee {
         this.salary = salary;
         id = counter++;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartments() {
+        return departments;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartments(int departments) {
+        this.departments = departments;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник под номером: " + id + " По имени: " + fullName + " Отдела: " + departments + " Получает " + salary + " рублей";
+    }
 }
